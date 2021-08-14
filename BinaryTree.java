4b0263428555eecf1dae7f372e47e1c730b1dc8d
moshtab <mdmohsin41439@gmail.com>
2021-08-14 11:19:59 +0530

    Added msg

---
+++ BinaryTree.java
@@ -0,0 +1,11 @@
+package day15_Tree;
+
+
+
+public class BinaryTree {
+
+	public static void main(String[] args) {
+	    System.out.println("Welcome to Binary search Tree computation program");
+	}
+
+}
